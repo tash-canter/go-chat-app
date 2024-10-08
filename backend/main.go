5a289f2287ebdae6d8ec80755fc7c036d4b89ea7
Tash <natashacanter@gmail.com>
2024-10-08 11:54:26 +0100

    Currently in working basic condition

--- backend/main.go
+++ backend/main.go
@@ -3,15 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/tash-canter/go-chat-app/backend/pkg/websocket"
 )
 
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	ws, err := websocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Fprintf(w, "%+V\n", err)
+	}
+	go websocket.Writer(ws)
+	websocket.Reader(ws)
+}
+
 func setupRoutes() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Simple Server")
-	})
+	http.HandleFunc("/ws", serveWs)
 }
 
 func main() {
+	fmt.Println("Distributed Chat App v0.01")
 	setupRoutes()
 	http.ListenAndServe(":8080", nil)
 }
